59112f968c65a9f9b11cf888f2dbfcc32f7ddcb7
jinheeyi <jinhee.yi27@gmail.com>
2020-01-29 22:03:29 +1000

    Practice - 1

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppLoading } from "expo";
+import * as Font from 'expo-font'
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+  state = {
+    loaded: false
+  };
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+
+  render() {
+    const { loaded } = this.state;
+    if (loaded) {
+      return (
+        <View>
+          <Text>Open up App.js to start working on your app</Text>
+        </View>
+      );
+
+    } else {
+      return (
+        <View>
+          <Text>3a Burton street, Harristown</Text>
+        </View>
+      );
+    }
+ 
+ 
+  }
 }
-
