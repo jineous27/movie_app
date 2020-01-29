ec08e65ac5277e410a5a866e1297ec8e5bb05416
jinheeyi <jinhee.yi27@gmail.com>
2020-01-29 22:21:47 +1000

    Add screens

---
+++ screens/Movies.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Movies extends Component {
+  render() {
+    return (
+        <View>
+            <Text>Movies</Text>
+        </View>
+    );
+  }
+}
---
+++ screens/TV.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class TV extends Component {
+  render() {
+    return (
+        <View>
+            <Text>TV</Text>
+        </View>
+    );
+  }
+}
