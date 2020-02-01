93a4835c2c6f6febe32205b6e09e1cca78c46995
jinheeyi <jinhee.yi27@gmail.com>
2020-02-02 09:08:17 +1000

    Add touchable opacity in movies

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native'; //리엑트네이트에 컴포넌트 TouchableOpacity 추가해줘야 밑에서 사용가능//
 
-export default class Movies extends Component {
-  render() {
-    return (
-        <View>
-            <Text>Movies</Text>
-        </View>
-    );
-  }
-}
+export default ({ navigation }) => (
+    <>
+      <Text>Movies</Text>
+      <TouchableOpacity onPress={()=> navigation.navigate("Detail")}>
+        <Text>Go to detail</Text>
+      </TouchableOpacity>
+    </>
+)
